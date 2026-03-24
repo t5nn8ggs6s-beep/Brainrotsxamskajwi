ceebe9ed364657ace2a966e1a2e03ad2b9a0aabe
t5nn8ggs6s-beep <t5nn8ggs6s@privaterelay.appleid.com>
2026-03-24 22:25:14 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+TOKEN = "8667484757:AAFX4-82fWAmsKHYKy_xAF7V9anc_ILCi5Y"
+CHANNEL = "@hapishops"        # канал для проверки подписки
+ADMINS = [8786867158]            # список Telegram ID админов
+CURRENCY = "XTR"                # Telegram Stars
